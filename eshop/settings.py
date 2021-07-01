4ffc84e4d317252737b1a045cd3952d6f33eb938
ChethanHK <chethanhk2000@gmail.com>
2021-07-01 23:22:03 +0530

    edit

--- eshop/settings.py
+++ eshop/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['eshop-appie.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['eshop-appie.herokuapp.com','localhost','127.0.0.1.8000']
 
 
 # Application definition
